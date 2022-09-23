8c283a887297de0663b683846e11bf307d8d2c2d
Your Name~ <you@example.com>
2022-09-23 08:56:55 +0300

    Updates

---
+++ precache-manifest.515dbb2a9f5333fe5c2eb949fa161f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7361df7f1ddef408b681",
+    "url": "/react-bookList/static/css/main.a81b2528.chunk.css"
+  },
+  {
+    "revision": "7361df7f1ddef408b681",
+    "url": "/react-bookList/static/js/main.7361df7f.chunk.js"
+  },
+  {
+    "revision": "d81810a4e64de93cb072",
+    "url": "/react-bookList/static/js/1.d81810a4.chunk.js"
+  },
+  {
+    "revision": "3b7989c4158bff84f9ae",
+    "url": "/react-bookList/static/js/runtime~main.3b7989c4.js"
+  },
+  {
+    "revision": "c2ffee5a2b564a0d2ca49ee1122c7f22",
+    "url": "/react-bookList/index.html"
+  }
+];
